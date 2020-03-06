68b0d7e05c3f317e3e4e6130664d2ffc00ce8a18
Doniyor Dadakhonov <doniyor.dadaxonov@gmail.com>
2020-03-06 18:00:56 +0500

    first commit

---
+++ precache-manifest.5b03a669079c2977cb45bd7bb49a3d30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e043355ac5c4de66791e",
+    "url": "/static/css/main.0c41c7c8.chunk.css"
+  },
+  {
+    "revision": "e043355ac5c4de66791e",
+    "url": "/static/js/main.e043355a.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b039324c47a57e8fb351",
+    "url": "/static/js/2.b039324c.chunk.js"
+  },
+  {
+    "revision": "db2f82d2d9efabc6251534df30f81b99",
+    "url": "/index.html"
+  }
+];
